ce1050fbe77fc5902851950df24adfab429dcb51
GitMamut <vatier@gazeta.pl>
2018-11-06 21:38:20 +0100

    add fixed rate of 10 minutes

--- src/main/java/mintfrost/cloud/persistance/DisplayToOled.java
+++ src/main/java/mintfrost/cloud/persistance/DisplayToOled.java
@@ -28,11 +28,19 @@ public class DisplayToOled implements Persister {
         headers.add(HttpHeaders.ACCEPT, "*/*");
 
 
+
+        HttpEntity<String> requestEntity1 = new HttpEntity<>("", headers);
+        final String targetUrl1 = "http://192.168.1.25/control?cmd=OLED,3,1,"+currentOutdoorResponse.getDate().replace("-","");
+        System.out.println("RQ: " + targetUrl1);
+        ResponseEntity<String> responseEntity1 = REST_TEMPLATE.exchange(targetUrl1, HttpMethod.GET, requestEntity1, String.class);
+        System.out.println("RS: " + responseEntity1);
+
         HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
         final String targetUrl = "http://192.168.1.25/control?cmd=OLED,4,1,Outside: "+currentOutdoorResponse.getValue().replace('.','/');
         System.out.println("RQ: " + targetUrl);
         ResponseEntity<String> responseEntity = REST_TEMPLATE.exchange(targetUrl, HttpMethod.GET, requestEntity, String.class);
         System.out.println("RS: " + responseEntity);
+
     }
 
     private List<CurrentOutdoorResponse> unmarshall(final ResponseEntity<String> responseEntity) {
